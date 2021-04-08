e7a30fbc353bec094b4169d8e2b4c05f3a103984
Taylor <taylor@eightfold.io>
2021-04-08 10:37:50 -0700

    poker proxy

--- poker-proxy.ts
+++ poker-proxy.ts
@@ -5,16 +5,11 @@ const URL_BASE = "https://poker.hyprtxt.dev"
 async function handleRequest(request: any) {
   const { pathname } = new URL(request.url)
   console.log(`Received request #${++count} to ${pathname}`)
-  if (pathname.startsWith("/proxy/")) {
+  if (pathname.startsWith("/")) {
     const diced = pathname.split("/")
-    const stuff = diced.slice(2, diced.length).join("/")
+    const stuff = diced.slice(1, diced.length).join("/")
     return stuff ? serveThing(stuff) : serveThing("/index.html")
   }
-  return new Response(JSON.stringify(Deno.env.toObject(), null, 2), {
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-    },
-  })
 }
 
 const serveThing = async (path: string) => {
